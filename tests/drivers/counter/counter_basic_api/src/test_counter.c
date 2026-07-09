96611ec0ef402fd5ef26bb6f43be3bc0ce082805
Aman Lachhiramka <a-lachhiramka@ti.com>
2026-07-09 15:13:23 +0530

    tests: drivers: counter: basic_api: Add MSPM0 to reliable cancel capable
    Mark MSPM0 counter as supporting reliable alarm cancellation following
    verified hardware behavior.
    
    Signed-off-by: Aman Lachhiramka <a-lachhiramka@ti.com>

--- tests/drivers/counter/counter_basic_api/src/test_counter.c
+++ tests/drivers/counter/counter_basic_api/src/test_counter.c
@@ -1292,6 +1292,11 @@ static bool reliable_cancel_capable(const struct device *dev)
 		return true;
 	}
 #endif
+#ifdef CONFIG_COUNTER_MSPM0_TIMER
+	if (single_channel_alarm_capable(dev)) {
+		return true;
+	}
+#endif
 	return false;
 }
 
